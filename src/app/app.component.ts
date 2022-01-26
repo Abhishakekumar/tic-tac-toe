67fc02a7de606253c6d95612c6988ec24e0ca61a
Y Abhishake Kumar <abhishakekumar51@gmail.com>
2022-01-26 21:30:13 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tic-tac-toe';
+
+  winMessage: string = '';
+  isCross = false;
+  itemArray:string[] = new Array(9).fill('empty')
+  count : number = 0;
+
+  constructor(private toastr:ToastrService){}
+
+  handleClick = (itemNumber: number) => {
+    if(this.winMessage) return this.toastr.success(this.winMessage);
+    
+    if(this.itemArray[itemNumber] !== 'empty') return this.toastr.info('Already filled')
+
+    this.itemArray[itemNumber] = this.isCross ? 'cross' : 'circle';
+    this.isCross = !this.isCross;
+    this.count++;
+    this.checkWinner();
+    if(this.count === 9) return this.winMessage = "All slots are filled"
+    return '';
+  }
+
+  checkWinner = () => {
+    if(this.itemArray[0] === this.itemArray[1] && this.itemArray[0] === this.itemArray[2] &&this.itemArray[0] !== 'empty') this.winMessage = `${this.itemArray[0]} won`;
+    else if(this.itemArray[3] !== 'empty' && this.itemArray[3] === this.itemArray[4] && this.itemArray[4] === this.itemArray[5]) this.winMessage = `${this.itemArray[3]} won`;
+    else if(this.itemArray[6] !== 'empty' && this.itemArray[6] === this.itemArray[7] && this.itemArray[7] === this.itemArray[8]) this.winMessage = `${this.itemArray[6]} won`;
+    else if(this.itemArray[0] !== 'empty' && this.itemArray[0] === this.itemArray[3] && this.itemArray[3] === this.itemArray[6]) this.winMessage = `${this.itemArray[0]} won`;
+    else if(this.itemArray[1] !== 'empty' && this.itemArray[1] === this.itemArray[4] && this.itemArray[4] === this.itemArray[7]) this.winMessage = `${this.itemArray[1]} won`;
+    else if(this.itemArray[2] !== 'empty' && this.itemArray[2] === this.itemArray[5] && this.itemArray[5] === this.itemArray[8]) this.winMessage = `${this.itemArray[2]} won`;
+    else if(this.itemArray[0] !== 'empty' && this.itemArray[0] === this.itemArray[4] && this.itemArray[4] === this.itemArray[8]) this.winMessage = `${this.itemArray[0]} won`;
+    else if(this.itemArray[2] !== 'empty' && this.itemArray[2] === this.itemArray[4] && this.itemArray[4] === this.itemArray[6]) this.winMessage = `${this.itemArray[2]} won`;
+  }
+
+  reloadGame = () => {
+    this.winMessage = '';
+    this.isCross = false;
+    this.itemArray = new Array(9).fill('empty');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IconComponent } from './components/icon/icon.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule, 
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
